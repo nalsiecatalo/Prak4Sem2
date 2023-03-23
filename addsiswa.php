d1c409d18ec72ff0c65ae824832c55785ba8f12b
nalsiecatalo <nabilaputrisyahida3@gmail.com>
2023-03-23 23:04:33 +0700

    Prak4Sem2

---
+++ addsiswa.php
@@ -0,0 +1,34 @@
+<?php
+    include_once 'template/header.php';
+?>
+
+<form>
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="text" name="text" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text1" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text1" name="text1" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+<?php
+    include_once 'template/footer.php'
+?>
